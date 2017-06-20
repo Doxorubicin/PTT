52cc5ec333b26008f997062b47a74a961b68eff7
Doxorubicin <apixaban0824@gmail.com>
2017-06-20 20:23:37 +0800

    外行人
    外行人，但自己可以用。
    目前較大的問題是檔案執行目錄預設是在Z:\\下面，但我懶得改，想說上傳來這邊，拋磚引玉。

---
+++ merge.py
@@ -0,0 +1,82 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 19 20:58:01 2017
+
+@author: ATPX4869
+"""
+
+from bs4 import BeautifulSoup
+import time
+
+
+def data(r=0):
+
+    # 網頁(html)在入soup中
+    soup = BeautifulSoup(r, 'lxml')
+    ddata = soup.find_all(class_ = 'article-meta-value')
+        
+    #[<span class="article-meta-value">bgworld (solid)</span>, <span class="article-meta-value">ac_in</span>, <span class="article-meta-value">[洽特] 路線擬人化</span>, <span class="article-meta-value">wed jun 18 07:41:36 2014</span>]
+    
+    board = 0
+    
+    #因為句子是逐行出現，所以只好這樣子土法煉鋼，讓它一次一次的對應。
+    number = 1
+    for div in ddata:
+        pddata = div.get_text()
+        if number == 1:
+            author = pddata
+        elif number == 2:
+            board = pddata
+        elif number == 3:
+            title = pddata
+        elif number == 4:
+            clock = pddata
+        number = number + 1
+        
+    #Python時間處理完全手冊
+    trans = clock
+    struct_time = time.strptime(trans,'%a %b %d %H:%M:%S %Y')
+    clock =  time.strftime('%Y-%m-%d(%a)%H-%M-%S',struct_time)
+    
+    #這是因為windows有些字元不能當檔名
+    #replace的用法
+    #\\	表示反斜線\
+    title = title.replace('*','＊')
+    title = title.replace('|','｜')
+    title = title.replace('\\','＼')
+    title = title.replace(':','：')
+    title = title.replace('"','＂')
+    title = title.replace('<','＜')
+    title = title.replace('>','＞')
+    title = title.replace('?','？')
+    title = title.replace('/','／')  
+    author = author.replace('*','＊')
+    author = author.replace('|','｜')
+    author = author.replace('\\','＼')
+    author = author.replace(':','：')
+    author = author.replace('"','＂')
+    author = author.replace('<','＜')
+    author = author.replace('>','＞')
+    author = author.replace('?','？')
+    author = author.replace('/','／')
+    
+    #檢查頁面是否還存在
+    if board == 0:
+        out =  "error:下面的網址已經404：" + time.strftime("%h:%m:%s")
+        with open('z:\\log.txt', 'a', encoding='utf-8') as log:
+            print(out, file=log)
+        raise ('網頁已經404')        
+    else:
+        author = "【" + author + "】"
+        board = "【" + board + "】"
+        title = title
+        clock = "【" + clock + "】"
+        name = clock + board + title + author
+        
+        file = "z:\\postPTT\\" + name + ".html"
+        out =  r
+        #open的指令用w，新建檔案寫入(檔案可不存在，若存在則清空)
+        with open(file, 'w', encoding='utf-8') as log:
+            print(out, file=log)  
+    return
+    
---
+++ start.py
@@ -0,0 +1,88 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 19 18:23:11 2017
+
+@author: ATPX4869
+"""
+
+
+import requests
+import time
+import merge
+
+def get_web_page(url):
+    #避免網站block掉機器人的流量
+    time.sleep(timeout)
+    resp = requests.get(
+        url=url,
+        cookies={'over18': '1'}
+    )
+    if resp.status_code != 200:
+        out = str(count) + "："  + url + "下載失敗404-" + time.strftime("%H:%M:%S")
+        with open('Z:\\log.txt', 'a', encoding='UTF-8') as log:
+            print(out, file=log)
+        return None
+    else:
+        return resp.text
+
+timeout = 0.5
+count = 1
+
+file = open("Z:\\weblist.txt","r")
+urlList = file.readlines()
+#行數
+lines = len(urlList)
+for url in urlList:
+    #進度
+    state = "（" + str(count) + "/" + str(lines) + "）"
+    #剩下幾次
+    poststate = lines - count
+    #還有多久(秒)
+    postsecond = poststate * timeout
+    #時間轉換
+    minute, second = divmod(postsecond, 60)
+    hour, minute = divmod(minute, 60)
+    day, hour = divmod(hour, 24)
+    
+    #進度
+    state = "（" + str(count) + "/" + str(lines) + "）"
+    #剩下幾次
+    poststate = lines - count
+    #還有多久(秒)
+    postsecond = poststate * timeout
+    #時間轉換
+    minute, second = divmod(postsecond, 60)
+    hour, minute = divmod(minute, 60)
+    day, hour = divmod(hour, 24)
+    
+    second = int(second)
+    minute = int(minute)
+    hour = int(hour)
+    day = int(day)
+    
+    #時間統整
+    if day == hour == minute == 0:
+        posttime = "還剩下：" + str(second) + "秒"
+    elif day == hour == 0:
+        posttime = "約：" + str(minute) + "分" + str(second) + "秒"
+    elif day == 0:
+        posttime = "約：" + str(hour) + "時" + str(minute) + "分" + str(second) + "秒"
+    else:
+        posttime = "約：" + str(day) + "天" + str(hour) + "時" + str(minute) + "分" + str(second) + "秒"
+    #公告
+    print ("目前進度：" + state + "還剩下" + str(poststate) + "次，" + posttime)
+    count = count + 1
+    """把字串中的\n(換行)給剔除"""
+    url=url.strip('\n')
+    try:
+        r = get_web_page(url)
+        out = r
+        merge.data(out)
+    except:
+        out = str(count) + "：" + url + "下載失敗-" + time.strftime("%H:%M:%S")
+        with open('Z:\\log.txt', 'a', encoding='UTF-8') as log:
+            print(out, file=log)
+    else:
+        out = str(count) + "："  + url + "下載成功-" + time.strftime("%H:%M:%S")
+        with open('Z:\\log.txt', 'a', encoding='UTF-8') as log:
+            print(out, file=log)
